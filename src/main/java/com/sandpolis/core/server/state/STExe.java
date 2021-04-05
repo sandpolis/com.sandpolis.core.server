6f4c1902699cb8067cc9aab2f764d6dd9623f603
Tyler Cook <tcc@sandpolis.com>
2021-04-04 19:59:32 -0500

    Add logging to STExe

--- src/main/java/com/sandpolis/core/server/state/STExe.java
+++ src/main/java/com/sandpolis/core/server/state/STExe.java
@@ -13,6 +13,9 @@ import static com.sandpolis.core.foundation.util.ProtoUtil.begin;
 import static com.sandpolis.core.foundation.util.ProtoUtil.success;
 import static com.sandpolis.core.instance.state.STStore.STStore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.google.protobuf.MessageLiteOrBuilder;
 import com.sandpolis.core.instance.state.oid.AbsoluteOid;
 import com.sandpolis.core.instance.state.st.STDocument;
@@ -20,14 +23,17 @@ import com.sandpolis.core.net.exelet.Exelet;
 import com.sandpolis.core.net.exelet.ExeletContext;
 import com.sandpolis.core.net.msg.MsgState.RQ_STSnapshot;
 import com.sandpolis.core.net.msg.MsgState.RQ_STSync;
-import com.sandpolis.core.net.state.STCmd.STSyncStruct;
 import com.sandpolis.core.net.state.st.entangled.EntangledDocument;
 
 public final class STExe extends Exelet {
 
+	private static final Logger log = LoggerFactory.getLogger(STExe.class);
+
 	@Handler(auth = false)
 	public static MessageLiteOrBuilder rq_st_snapshot(RQ_STSnapshot rq) {
 		var oid = new AbsoluteOid<STDocument>(null, rq.getOid());
+
+		log.debug("Received snapshot request for OID: {}", oid);
 
 		return STStore.root().document(oid).snapshot();
 	}
@@ -36,16 +42,17 @@ public final class STExe extends Exelet {
 	public static MessageLiteOrBuilder rq_st_sync(ExeletContext context, RQ_STSync rq) {
 		var outcome = begin();
 
-		var config = new STSyncStruct();
-		config.connection = context.connector;
-		config.direction = rq.getDirection();
-		config.streamId = rq.getStreamId();
-		config.updatePeriod = rq.getUpdatePeriod();
-		config.initiator = false;
-
 		var oid = new AbsoluteOid<STDocument>(null, rq.getOid());
 
-		new EntangledDocument(STStore.root().document(oid), config);
+		log.debug("Received sync request for OID: {}", oid);
+
+		new EntangledDocument(STStore.root().document(oid), config -> {
+			config.connection = context.connector;
+			config.direction = rq.getDirection();
+			config.streamId = rq.getStreamId();
+			config.updatePeriod = rq.getUpdatePeriod();
+			config.initiator = false;
+		});
 
 		return success(outcome);
 	}
